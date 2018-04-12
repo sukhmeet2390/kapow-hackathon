b415d4eace8cee580799c34548198d206a7559f9
himanshujaju <himanshujaju@kapow.games>
2018-04-12 18:45:26 +0530

    Added projectile code

--- src/states/Main.js
+++ src/states/Main.js
@@ -2,20 +2,19 @@ import ExampleObject from 'objects/ExampleObject';
 
 class Main extends Phaser.State {
 
-	create() {
-		console.log("Game started");
-		//Enable Arcade Physics
+	preload() {
 		this.game.physics.startSystem(Phaser.Physics.ARCADE);
+		this.game.physics.arcade.gravity.y = 400;
+	}
 
-		//Set the games background colour
-		this.game.stage.backgroundColor = '#000000';
-
-		//Example of including an object
-		//let exampleObject = new ExampleObject(this.game);
+	create() {
+		this.car = this.game.add.sprite(100, 1000, 'car');
+		this.game.physics.enable([this.car], Phaser.Physics.ARCADE);
+		this.car.body.collideWorldBounds = true;
+		this.game.physics.arcade.velocityFromAngle(-45, 700, this.car.body.velocity);
 	}
 
 	update() {
-		
 	}
 
 }
--- src/states/Preload.js
+++ src/states/Preload.js
@@ -2,7 +2,7 @@ class Preload extends Phaser.State {
 
 	preload() {
 		/* Preload required assets */
-		//this.game.load.image('myImage', 'assets/my-image.png');
+		this.game.load.image('car', 'assets/car.png');
 		//this.game.load.audio('myAudio', 'assets/my-audio.wav');
 		//this.game.load.atlas('myAtlas', 'assets/my-atlas.png', 'assets/my-atlas.json');
 	}
