b2a3d452b19b2c6df8f024a6ccb7f90233122d7b
himanshujaju <himanshujaju@kapow.games>
2018-04-14 18:10:18 +0530

    added history parser

--- src/kapow/HistoryWrapper.js
+++ src/kapow/HistoryWrapper.js
@@ -3,18 +3,27 @@
 import kapowWrapper from "./KapowWrapper"
 
 let HistoryWrapper = {
-    getChoice(){
-        return 1;
-    },
     getAllMessages(successCB) {
         kapowWrapper.fetchAllHistory(successCB);
+    },
+    getChoice(playerID, successCB){
+        getAllMessages(function(messages) {
+            let n = messages.length;
+            for (var i = 0; i < n; i++) {
+                let message = messages[i];
+                if (message.type == "move" && message.data.type == "CharacterChosen" && message.data.chooserId == playerID) {
+                    successCB(message);
+                }
+            }
+            successCB(null);
+        });
     },
     getLastMove(successCB) {
         getAllMessages(function(messages) {
             let n = messages.length;
             for (var i = 0; i < n; i++) {
                 let message = messages[i];
-                if (message.type == "move") {
+                if (message.type == "move" && message.data.type == "Move") {
                     successCB(message);
                 }
             }
